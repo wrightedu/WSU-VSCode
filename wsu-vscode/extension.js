b445bb612685a84aeb70d7879ae3742c62c8155c
Owen O'Connor <oconnor.57@wright.edu>
2020-02-14 15:53:34 -0500

    Starting to add support to automatically run commands

--- wsu-vscode/extension.js
+++ wsu-vscode/extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const sys = require('child_process');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,14 +18,33 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	let helloWSU = vscode.commands.registerCommand('extension.helloWSU', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('Raider up!');
 	});
 
-	context.subscriptions.push(disposable);
+	let helloConsole = vscode.commands.registerCommand('extension.helloConsole', function () {
+		let cmds = ['echo hi', 'fortune', 'false', 'feriotgoetihogd'];
+
+		cmds.forEach((i) => {
+			sys.exec('bash -c ' + i,  (err, stdout, stderr) => {
+				if(err) {
+					vscode.window.showInformationMessage("command " + i + " threw an error: " +stderr);
+				}
+
+				else {
+					vscode.window.showInformationMessage("command " + i + " ran succesfully");
+				}
+			});
+		});
+
+		vscode.window.showInformationMessage("Done running commands");
+	});
+
+	context.subscriptions.push(helloConsole);
+	context.subscriptions.push(helloWSU);
 }
 exports.activate = activate;
 
